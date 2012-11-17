b4479a330a01cb87ff12943f633463b8e43fb5c5
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2012-11-17 10:08:45 +0200

    Python 3 compatibility

--- stats/generate.py
+++ stats/generate.py
@@ -26,7 +26,7 @@ from django.conf import settings
 
 from django.core.exceptions import ObjectDoesNotExist
 from mercurial import hg, ui
-from models import ChangeSet, HGUser
+from .models import ChangeSet, HGUser
 
 
 try:
@@ -36,28 +36,28 @@ except ObjectDoesNotExist:
 
 repo = hg.repository(ui.ui(), settings.REPO_PATH)
 tip = repo.revs("default")[0]
-print "Fetching revisions {0}-{1}".format(start, tip)
+print("Fetching revisions {0}-{1}".format(start, tip))
 for rev in range(start, tip):
     adds = removes = 0
     ctx = repo.changectx(rev)
     user, _ = HGUser.objects.get_or_create(name=ctx.user())
     timestamp = datetime.fromtimestamp(ctx.date()[0]).isoformat()
     if len(ctx.parents()) > 1:
-        print "skipping merge {0}".format(rev)
+        print("skipping merge {0}".format(rev))
         continue
     for filecount, diff in enumerate(ctx.diff()):
         diffcmd, diffdata = diff.split('\n', 1)
         #print diffcmd
         if "3rdparty" in diffcmd:
-            print "skipping 3rd party component {0}".format(
-                diffcmd.rsplit(' ', 1)[-1])
+            print("skipping 3rd party component {0}".format(
+                diffcmd.rsplit(' ', 1)[-1]))
             continue
             #print fromfile.rsplit(' ', 1)[-1]
         #print tofile.rsplit(' ', 1)[-1]
         for line in diffdata:
             if line.startswith('Binary'):
-                print "Skipping binary file".format(
-                    diffcmd.rsplit(' ', 1)[-1])
+                print("Skipping binary file".format(
+                    diffcmd.rsplit(' ', 1)[-1]))
                 break
             if line.startswith('+'):
                 adds += 1
@@ -71,4 +71,4 @@ for rev in range(start, tip):
                           files=filecount,
                           description=ctx.description())
     changeset.save()
-    print "{0},'{1}',{2},{3}".format(timestamp, user.name, adds, removes)
+    print("{0},'{1}',{2},{3}".format(timestamp, user.name, adds, removes))
