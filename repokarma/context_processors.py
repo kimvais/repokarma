69ec868495b8ceef60a262fecc1cf3e7ebd09ee1
kimvais <kimvais@ssh.com>
2013-08-08 21:29:17 +0300

    Use my default css & base template

--- repokarma/context_processors.py
+++ repokarma/context_processors.py
@@ -24,5 +24,8 @@ from django.conf import settings
 
 
 def base(request):
-    context = dict(repolocation=os.path.abspath(settings.REPO_PATH))
+    context = dict(repolocation=os.path.abspath(settings.REPO_PATH),
+                   COPYRIGHTYEAR=2013,
+                   AUTHOR_NICK="kivmais",
+                   AUTHOR_NAME="Kimmo Parviainen-Jalanko")
     return context
